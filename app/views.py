9faa47b6aba6336b9af87531fab618b114408b60
itjunky <itjunky@github.com>
2023-08-24 01:21:55 +0300

    Show task list and add new

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import tasker
 from .forms import NewTaskForm
+from .common import createnewtask, tasklist
 from flask import render_template, redirect
+
 
 @tasker.route('/')
 @tasker.route('/index')
@@ -12,14 +14,17 @@ def index():
     # TODO помечать задачу выполненной
 @tasker.route('/tasklist')
 def show_tasks():
-    return render_template('tasklist.html')
+    return render_template('tasklist.html', tasklist=tasklist())
 
 
-# TODO Добавление задачи (форма ввода)
+# TODO Добавление задачи (сохранение в БД)
 @tasker.route('/newtask', methods=['GET', 'POST'])
 def create_new_task():
     form = NewTaskForm()
     if form.validate_on_submit():
+        print(form.title.data)
+        print(form.body.data)
+        createnewtask(form.title.data, form.body.data)
         return redirect('/tasklist')
     return render_template('newtask.html', form=form)
 
