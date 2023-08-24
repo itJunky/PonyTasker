f6c65b0301fb0b73ef3d78af2dd9aefe13db09ba
itjunky <itjunky@github.com>
2023-08-24 15:24:02 +0300

    refactoring

--- app/common.py
+++ app/common.py
@@ -1,16 +1,13 @@
-from pony.orm import db_session, select
-from .models import db, Task
+from pony.orm import select
+from .models import Task
 
 
-# @db_session
 def createnewtask(title, body):
     new_task = Task(title=title,
                     body=body,
                     completion=False)
-    # db.commit()
     return print('ok')
 
 
-# @db_session
 def tasklist():
     return list(select(t for t in Task))
