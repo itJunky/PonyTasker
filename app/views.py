98dfba01ac1fe5a8cca1a8a745c783e552f90516
itjunky <itjunky@github.com>
2023-08-24 15:28:59 +0300

    Task page and back

--- app/views.py
+++ app/views.py
@@ -1,32 +1,44 @@
 from app import tasker
 from .forms import NewTaskForm
+from .models import Task
 from .common import createnewtask, tasklist
 from flask import render_template, redirect
+from pony.orm import select
 
 
 @tasker.route('/')
 @tasker.route('/index')
 def index():
-    return "Hello, Worldx!</br><a href=/newtask>Новая задача</a>"
+    return "Hello, it's a PonyTasker!</br>" \
+           "<a href=/tasklist>Список задач</a></br>" \
+           "<a href=/newtask>Новая задача</a>"
 
 
-# TODO Список задач
+# Список задач
     # TODO помечать задачу выполненной
 @tasker.route('/tasklist')
 def show_tasks():
     return render_template('tasklist.html', tasklist=tasklist())
 
 
-# TODO Добавление задачи (сохранение в БД)
+# Добавление задачи
 @tasker.route('/newtask', methods=['GET', 'POST'])
 def create_new_task():
     form = NewTaskForm()
     if form.validate_on_submit():
-        print(form.title.data)
-        print(form.body.data)
         createnewtask(form.title.data, form.body.data)
         return redirect('/tasklist')
     return render_template('newtask.html', form=form)
 
-# TODO Отображение самой задачи
+
+# Отображение самой задачи
+@tasker.route('/task/<int:task_id>')
+def show_task(task_id):
+    t = select(t for t in Task if t.id == task_id)[:]
+    print(type(t))
+    print(t)
+    return render_template('task.html', task=t[0])
+
 # TODO Редактирование задачи
+
+
